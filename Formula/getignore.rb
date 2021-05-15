01d739b174b6d56e94d073519ca137b944f0ec2f
Kshitij Chauhan <kshitijchauhan1105@outlook>
2021-05-15 17:09:25 +0530

    fix: Remove version from formula

--- Formula/getignore.rb
+++ Formula/getignore.rb
@@ -2,7 +2,6 @@ class Getignore < Formula
   desc "Fetch gitignore files for your projects right from the command-line"
   homepage "https://github.com/haroldadmin/getignore"
   url "https://github.com/haroldadmin/getignore/archive/refs/tags/0.0.2.tar.gz"
-  version "0.0.2"
   sha256 "f4424c57f401e8c313749ba236954ac07738344bb943bf4c8a0d89bc58ec6b28"
   license "Apache-2.0"
 

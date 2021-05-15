ee421f830129a3f7d76e7afef378cca4b145d349
Kshitij Chauhan <kshitijchauhan1105@outlook>
2021-05-15 14:48:32 +0530

    fix: Fix more offenses

--- Formula/getignore.rb
+++ Formula/getignore.rb
@@ -1,8 +1,8 @@
 class Getignore < Formula
   desc "Fetch gitignore files for your projects right from the command-line"
   homepage "https://github.com/haroldadmin/getignore"
-  version "0.0.1"
   url "https://github.com/haroldadmin/getignore/releases/download/0.0.1/getignore-darwin-amd64"
+  version "0.0.1"
   sha256 "747fd55b3a5dcaf3934c66fbe7dcbd59e13ef3e98f4a89a5949f1dcc8918c6fc"
   license "Apache-2.0"
 

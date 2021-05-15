f97ca21882d0ebaa1c6acf4794b40b13fb33c58b
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2021-05-15 11:58:41 +0000

    getignore: add 0.0.2 bottle.

--- Formula/getignore.rb
+++ Formula/getignore.rb
@@ -5,6 +5,12 @@ class Getignore < Formula
   sha256 "f4424c57f401e8c313749ba236954ac07738344bb943bf4c8a0d89bc58ec6b28"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/haroldadmin/homebrew-getignore/releases/download/getignore-0.0.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "16c5eafa87debece376c0f1fa5a0ed342012e2c849a7390b096a4d93ff17fbd4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "aab5b2cba8f9b06c98b49739b1882bc16e17e2f473d7422e52bffa10d0769f82"
+  end
+
   depends_on "go" => :build
 
   def install

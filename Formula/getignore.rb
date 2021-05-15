60f261397936d65f80f0dfe1bc89c464815a3108
Kshitij Chauhan <kshitijchauhan1105@outlook>
2021-05-15 14:37:57 +0530

    feat: Add homepage

--- Formula/getignore.rb
+++ Formula/getignore.rb
@@ -1,6 +1,6 @@
 class Getignore < Formula
   desc "Fetch gitignore files for your projects right from the command line"
-  homepage ""
+  homepage "https://github.com/haroldadmin/getignore"
   url "https://github.com/haroldadmin/getignore/releases/download/0.0.1/getignore-darwin-amd64"
   sha256 "747fd55b3a5dcaf3934c66fbe7dcbd59e13ef3e98f4a89a5949f1dcc8918c6fc"
   license "Apache-2.0"
